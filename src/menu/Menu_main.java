6dbd95d13b2a36c4e7b4fa952bc949a2646e5d0a
50616032 <50616032@50616032-pc>
2018-01-21 23:50:57 +0900

    50616032

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu_view frame = new Menu_view();
+        frame.setBounds(5,5,655,455);
+        frame.setVisible(true);
+
+	}
+
+}
